404a6901225df098c8f217edde96110a76d9f6ad
Nathan Melenbrink <nathanmelenbrink@users.noreply.github.com>
2018-05-18 11:48:42 -0400

    removed isotope

--- js/index.js
+++ js/index.js
@@ -1,30 +1,43 @@
 $(window).load(function(){
     var $container = $('.gridder');
-    $container.isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false
-        }
-    });
+    // $container.isotope({
+    //     transformsEnabled: false,
+    //     itemSelector: '.gridder-list',
+    //     filter: '*'//,
+    //     // animationOptions: {
+    //     //     duration: 750,
+    //     //     easing: 'linear',
+    //     //     queue: false
+    //     // }
+    // });
  
     $('.portfolioFilter a').click(function(){
         $('.portfolioFilter .current').removeClass('current');
         $(this).addClass('current');
         var selector = $(this).attr('data-filter');
-        console.log(selector)
-        $container.isotope({
-            filter: selector,
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-         });
+        console.log(selector);
+
+        $("#portfolio").fadeTo(100, 0.1);
+        $("#portfolio li").not(""+selector).fadeOut().removeClass('scale-anm');
+        setTimeout(function() {
+          $(""+selector).fadeIn().addClass('scale-anm');
+          $("#portfolio").fadeTo(300, 1);
+        }, 300); 
+
+        // $container.isotope({
+        //     transformsEnabled: false,
+        //     itemSelector: '.gridder-list',
+        //     filter: selector//,
+        //     // animationOptions: {
+        //     //     duration: 750,
+        //     //     easing: 'linear',
+        //     //     queue: false
+        //     // }
+        //  });
          return false;
     }); 
 });
+
 
 // Hide all elements with class="containerTab", except for the one that matches the clickable grid column
 // function openTab(tabName) {
